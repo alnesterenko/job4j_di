a3b25e53bf7b59e9fa134c5876e70dd4a2f343e7
alnesterenko <alzpupkin@gmail.com>
2025-04-30 20:53:06 +0300

    2. DI, IoC, Beans, BeanFactory, ApplicationContext [#505070]

---
+++ src/main/java/ru/job4j/di/JavaContextDI.java
@@ -0,0 +1,16 @@
+package ru.job4j.di;
+
+import ru.job4j.di.model.StartUI;
+import ru.job4j.di.model.Store;
+
+public class JavaContextDI {
+    public static void main(String[] args) {
+        /* !!! Это псевдокод !!! */
+        /*var context = new Context();
+        context.searchInPackage("ru.job4j");
+        var ui = context.get(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();*/
+    }
+}

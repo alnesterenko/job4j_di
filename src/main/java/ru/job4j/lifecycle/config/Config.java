401a082f128e65c58f8d4ced696ae0a779aa6f1b
alnesterenko <alzpupkin@gmail.com>
2025-05-01 18:06:02 +0300

    3. Жизненный цикл Spring Bean [#505071]

---
+++ src/main/java/ru/job4j/lifecycle/config/Config.java
@@ -0,0 +1,19 @@
+package ru.job4j.lifecycle.config;
+
+
+import ru.job4j.lifecycle.model.Product;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean(name = "myProduct",
+            initMethod = "initMethod",
+            destroyMethod = "destroyMethod"
+    )
+    public Product getProduct() {
+        return new Product("Good Product", 2.5);
+    }
+}

62d75ff815db0ddeb6153c90ea89356ef2bd7d05
alnesterenko <alzpupkin@gmail.com>
2025-07-10 16:57:16 +0300

    1. Концепция AOP [#505076 #547997]. Без использования AOP.

---
+++ src/main/java/ru/job4j/aop/config/Config.java
@@ -0,0 +1,9 @@
+package ru.job4j.aop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ru.job4j.aop")
+public class Config {
+}

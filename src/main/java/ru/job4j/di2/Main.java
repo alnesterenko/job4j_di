142d4060ae089a6431c59073a496c5afcc5a597b
alnesterenko <alzpupkin@gmail.com>
2026-05-06 08:17:51 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di2/Main.java
+++ src/main/java/ru/job4j/di2/Main.java
@@ -1,16 +1,12 @@
 package ru.job4j.di2;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.job4j.di2.model.ConsoleInput;
 import ru.job4j.di2.model.StartUI;
-import ru.job4j.di2.model.Store;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(ConsoleInput.class);
-        context.register(StartUI.class);
+        context.scan("ru.job4j.di2");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
 

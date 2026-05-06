a04e39677c5fad1566ce1c984f6a699ce89f875b
alnesterenko <alzpupkin@gmail.com>
2026-05-06 09:28:43 +0300

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di2/SpringDI.java
+++ src/main/java/ru/job4j/di2/SpringDI.java
@@ -1,16 +1,32 @@
 package ru.job4j.di2;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.job4j.di2.model.StartUI;
+import ru.job4j.di2.model.Store;
+
+import java.util.List;
 
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di2");
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
+
+        /* Ниже код добавленный мной */
+        System.out.println("Содержимое store:");
+        printContent(store.getAll());
+        System.out.println("Содержимое another:");
+        printContent(another.getAll());
+    }
+
+    public static void printContent(List<String> contentList) {
+        if (contentList.isEmpty()) {
+            System.out.println("Содержимое отсутствует!");
+        } else {
+            contentList.forEach(System.out::println);
+        }
     }
 }
